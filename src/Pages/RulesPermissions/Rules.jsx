ed6296094de396409c80a1df5540c9c7c689c6a0
HosseinDev-02 <hosseinDev2002@gmail.com>
2025-03-10 00:37:52 +0330

    refactor style in index.css & rules page

--- src/Pages/RulesPermissions/Rules.jsx
+++ src/Pages/RulesPermissions/Rules.jsx
@@ -533,7 +533,10 @@ export default function Rules() {
                             '.css-1f63zk': {
                                 display: 'none !important'
                             },
-
+                            '.css-l45izh': {
+                                padding: '16px !important',
+                                justifyContent: 'space-between !important'
+                            }
                         }}
                         disableRowSelectionOnClick
                         disableColumnFilter
